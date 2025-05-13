97106ecb5477b95c2db1e71e5c95047f0d97d828
bugstile <bugstile@hotmail.com>
2025-05-13 17:00:05 +0200

    added swagger docs for airquality and gps

--- docs/airQualitySwagger.js
+++ docs/airQualitySwagger.js
@@ -2,45 +2,26 @@
  * @swagger
  * tags:
  *   name: AirQuality
- *   description: Air Quality monitoring
+ *   description: Air quality sensor data
  */
 
 /**
  * @swagger
  * /airQuality:
  *   get:
- *     summary: Fetch air quality data
+ *     summary: Get all air quality sensor entries
  *     tags: [AirQuality]
  *     responses:
  *       200:
- *         description: Successful response
+ *         description: List of all air quality readings
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: values were fetched
- *                 value:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         format: date-time
- *                       pm1:
- *                         type: string
- *                       pm2:
- *                         type: string
- *                       pm10:
- *                         type: string
- *                       tvoc:
- *                         type: string
- *
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/AirQuality'
  *   post:
- *     summary: Create air quality data
+ *     summary: Add a new air quality reading
  *     tags: [AirQuality]
  *     requestBody:
  *       required: true
@@ -48,38 +29,110 @@
  *         application/json:
  *           schema:
  *             type: object
+ *             required: [device_id, smoke, propane, co2]
  *             properties:
- *               pm1:
+ *               device_id:
  *                 type: string
- *               pm2:
- *                 type: string
- *               pm10:
- *                 type: string
- *               tvoc:
- *                 type: string
+ *               smoke:
+ *                 type: number
+ *               propane:
+ *                 type: number
+ *               co2:
+ *                 type: number
  *     responses:
  *       201:
- *         description: Data created successfully
+ *         description: Created air quality record
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AirQuality'
+ */
+
+/**
+ * @swagger
+ * /airQuality/{id}/latest-data:
+ *   get:
+ *     summary: Get latest air quality values for a device
+ *     tags: [AirQuality]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Device ID
+ *     responses:
+ *       200:
+ *         description: Latest air quality data
  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
- *                 message:
+ *                 device_id:
  *                   type: string
- *                   example: values fetched
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: string
- *                       format: date-time
- *                     pm1:
- *                       type: string
- *                     pm2:
- *                       type: string
- *                     pm10:
- *                       type: string
- *                     tvoc:
- *                       type: string
+ *                 latest_smoke:
+ *                   type: number
+ *                 latest_propane:
+ *                   type: number
+ *                 latest_co2:
+ *                   type: number
+ *                 latest_timestamp:
+ *                   type: string
+ *                   format: date-time
+ *       404:
+ *         description: No data found
+ */
+
+/**
+ * @swagger
+ * /airQuality/{id}/trend:
+ *   get:
+ *     summary: Get last 50 air quality readings for a device
+ *     tags: [AirQuality]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Device ID
+ *     responses:
+ *       200:
+ *         description: List of air quality readings
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 device_id:
+ *                   type: string
+ *                 trend:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/AirQuality'
+ *       404:
+ *         description: No trend data found
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AirQuality:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         device_id:
+ *           type: string
+ *         smoke:
+ *           type: number
+ *         propane:
+ *           type: number
+ *         co2:
+ *           type: number
+ *         created_at:
+ *           type: string
+ *           format: date-time
  */
--- docs/gpsSwagger.js
+++ docs/gpsSwagger.js
@@ -9,7 +9,7 @@
  * @swagger
  * /gps:
  *   get:
- *     summary: Fetch GPS data
+ *     summary: Fetch all GPS records
  *     tags: [GPS]
  *     responses:
  *       200:
@@ -17,25 +17,9 @@
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: values were fetched
- *                 value:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         format: date-time
- *                       latitude:
- *                         type: string
- *                         example: "59.3293"
- *                       longtitude:
- *                         type: string
- *                         example: "18.0686"
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/GPS'
  *
  *   post:
  *     summary: Create GPS data
@@ -46,34 +30,104 @@
  *         application/json:
  *           schema:
  *             type: object
+ *             required: [device_id, latitude, longitude]
  *             properties:
+ *               device_id:
+ *                 type: string
  *               latitude:
- *                 type: string
- *                 example: "59.3293"
- *               longtitude:
- *                 type: string
- *                 example: "18.0686"
+ *                 type: number
+ *               longitude:
+ *                 type: number
  *     responses:
  *       201:
  *         description: Data created successfully
  *         content:
  *           application/json:
  *             schema:
+ *               $ref: '#/components/schemas/GPS'
+ */
+
+/**
+ * @swagger
+ * /gps/{id}/latest-data:
+ *   get:
+ *     summary: Get latest GPS data for a device
+ *     tags: [GPS]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Device ID
+ *     responses:
+ *       200:
+ *         description: Latest GPS coordinates
+ *         content:
+ *           application/json:
+ *             schema:
  *               type: object
  *               properties:
- *                 message:
+ *                 device_id:
  *                   type: string
- *                   example: values received
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: string
- *                       format: date-time
- *                     latitude:
- *                       type: string
- *                       example: "59.3293"
- *                     longtitude:
- *                       type: string
- *                       example: "18.0686"
+ *                 latest_latitude:
+ *                   type: number
+ *                 latest_longitude:
+ *                   type: number
+ *                 latest_timestamp:
+ *                   type: string
+ *                   format: date-time
+ *       404:
+ *         description: No data found
+ */
+
+/**
+ * @swagger
+ * /gps/{id}/trend:
+ *   get:
+ *     summary: Get latest 50 GPS positions for a device
+ *     tags: [GPS]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Device ID
+ *     responses:
+ *       200:
+ *         description: GPS position history
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 device_id:
+ *                   type: string
+ *                 trend:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/GPS'
+ *       404:
+ *         description: No trend data found
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     GPS:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         device_id:
+ *           type: string
+ *         latitude:
+ *           type: number
+ *         longitude:
+ *           type: number
+ *         created_at:
+ *           type: string
+ *           format: date-time
  */

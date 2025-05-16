489b4bc9ceaa0c502b0acb53d0ed5a63687d7597
AndreasZetter <andreas.zetterlund98@gmail.com>
2025-05-16 10:18:20 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,8 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: procces.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-export default pool;

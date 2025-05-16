ad22c5b6b1d44884c3f58cc47cfffdc2f26adc70
andreaszetter <andreas.zetterlund98@gmail.com>
2025-05-16 09:47:30 +0200

    added third part db

--- models/db.js
+++ models/db.js
@@ -1,10 +1,6 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "chaschallenge",
-  password: "1234",
-  port: 5432,
+  connectionString: procces.env.CONNECTION_STRING
 });
 export default pool;

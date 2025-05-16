4f73cf7ee2c678e939456fb80efb82482a24d14d
andreaszetter <andreas.zetterlund98@gmail.com>
2025-05-16 12:23:42 +0200

    solved an issue where app wasnt correctly exported

--- docs/swagger.js
+++ docs/swagger.js
@@ -2,7 +2,7 @@ import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
 const swaggerOptions = {
-  swaggerDefinition: {
+  definition: {
     openapi: '3.0.0',
     info: {
       title: 'API Documentation',
